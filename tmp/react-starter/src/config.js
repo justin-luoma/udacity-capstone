0540b96a204332c15aa1af227e55ed7624d621ad
Justin Luoma <justin@justinluoma.com>
2022-07-08 09:12:36 -0400

    chore: remove starter code

--- tmp/react-starter/src/config.js
+++
@@ -1,8 +0,0 @@
-const apiId = 'i65si6ec8c'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
-
-export const authConfig = {
-    domain: 'dev-sz67ap41.us.auth0.com',
-    clientId: 'ITKpfwTGPrHyidIFIUIxmB0AhHLTQOZe',
-    callbackUrl: 'http://localhost:1234/callback'
-}
